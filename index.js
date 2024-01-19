b8684d649e01a295f0dfe3b15124c6fab3a2fe62
ALIEN'S <apple@ALIENS.local>
2024-01-19 16:50:18 +0100

    number-method

---
+++ index.js
@@ -0,0 +1,57 @@
+let number = 3;
+
+//toString
+let toString = number.toString(2);
+console.log(toString);
+
+//toexponential
+let toexponential = 1234;
+let resulttoexp = toexponential.toExponential(1);
+console.log(resulttoexp);
+
+//toprecision
+let num = 1283;
+let precision = num.toPrecision(2);
+console.log(precision);
+
+//tofixed
+let num2 = 2304;
+let fixed = num2.toFixed(2);
+console.log(fixed);
+
+//valueof
+let valnum = 555;
+let valueof = valnum.valueOf();
+console.log(valueof);
+
+//MATHS METHODS
+//pow
+let pow = Math.pow(10,7);
+console.log(pow);
+
+//sqrt
+let sqrt = Math.sqrt(625);
+console.log(sqrt);
+
+//min
+let min = Math.min(1,2,3,4,5);
+console.log(min);
+
+//random
+let random = Math.random();
+console.log(random);
+
+//floor
+let floor = Math.floor(464.22345678987);
+console.log(floor);
+
+//ceil
+let ceil = Math.ceil(484.22345678987);
+console.log(ceil);
+
+
+//round
+let round = Math.round(489.22345678987);
+console.log(round);
+
+
